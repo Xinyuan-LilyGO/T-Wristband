8a4a0d220311866c92aad36e0ef10ad1dcf5e2b1
liangyingy <1102390310@qq.com>
2022-11-08 15:42:08 +0800

    perfect: Optimize apply_patch.py
    Signed-off-by: liangyingy <1102390310@qq.com>

--- scripts/apply_patches.py
+++ scripts/apply_patches.py
@@ -1,15 +1,48 @@
-from os.path import join, isfile
-import os
+from os.path import join, abspath
+import logging
 
 Import("env")
 
+# try:
+#     import patch
+# except ImportError:
+#     env.Execute("$PYTHONEXE -m pip install git+https://github.com/liangyingy/python-patch.git")
+
+logging.basicConfig(level = logging.DEBUG, format='%(levelname)s %(message)s')
+logger = logging.getLogger('platformio-patch')
+
 project_dir = env['PROJECT_DIR']
-patch = join(project_dir, '..', '..', 'scripts', 'patch.py')
-patches_dir = join(project_dir, '..', '..', 'libdeps', 'patches')
+patch_exe = join(project_dir, '..', '..', 'scripts', 'patch.py')
 
-os.chdir(join(project_dir, '..', '..'))
+section = env.GetProjectOption('custom_patch_section')
+config = env.GetProjectConfig()
+root_dir = abspath(config.get(section, 'root_dir', default=join(project_dir)))
+patches = config.get(section, 'patches', default="").splitlines()
 
-for i in os.listdir(patches_dir):
-    env.Execute("python %s %s --debug" % (patch, join(patches_dir, i)))
+logger.info('root directory for applying patch: %s' % root_dir)
+logger.info('patch list:')
+for i in patches:
+    logger.info('  %s' % i)
 
-os.chdir(project_dir)
+for i in patches:
+    logger.info("patching %s" % (abspath(i)))
+    env.Execute("$PYTHONEXE %s %s --debug -d %s" % (patch_exe,
+                                                    abspath(i),
+                                                    root_dir
+                                                    )
+                )
+
+def post_program_action(source, target, env):
+    global root_dir
+    global patches
+    project_dir = env['PROJECT_DIR']
+    patch_exe = join(project_dir, '..', '..', 'scripts', 'patch.py')
+    for i in patches:
+        logger.info("unpatching %s" % abspath(i))
+        env.Execute("$PYTHONEXE %s %s --debug -d %s --revert" % (patch_exe,
+                                                                 abspath(i),
+                                                                 root_dir
+                                                                )
+                    )
+
+env.AddPostAction("$PROGPATH", post_program_action)
--- scripts/clear_patches.py
+++ scripts/clear_patches.py
@@ -5,8 +5,14 @@ Import("env")
 
 def post_program_action(source, target, env):
     project_dir = env['PROJECT_DIR']
-    os.chdir(join(project_dir, '..', '..', 'libdeps'))
-    env.Execute("git checkout .")
-    os.chdir(project_dir)
+    patches_dir = join(project_dir, '..', '..', 'libdeps', 'patches')
+    patch = join(project_dir, '..', '..', 'scripts', 'patch.py')
+    for i in os.listdir(patches_dir):
+        print("unpatching %s" % join(patches_dir, i))
+        env.Execute("$PYTHONEXE %s %s --debug -d %s --revert" % (patch,
+                                                                 join(patches_dir, i),
+                                                                 join(project_dir, '..', '..')
+                                                                )
+                    )
 
 env.AddPostAction("$PROGPATH", post_program_action)
